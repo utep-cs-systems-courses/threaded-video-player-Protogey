5b6000f9272c38eb9bbaf76c9b4b9d15588d9f61
student <protogeyorocks@gmail.com>
2020-11-24 18:18:48 -0700

    finished lab?

--- ExtractAndDisplay.py
+++ ExtractAndDisplay.py
@@ -6,6 +6,26 @@ import numpy as np
 import base64
 import queue
 
+class TObject:
+    def __init__(self):
+        self.queue = queue.Queue()#here we use a queue as our queue in the object
+        self.full = threading.Semaphore(0)#semaphore for keeping track if our queue is full
+        self.empty = threading.Semaphore(10)#semaphore for keeping track if we have an empty queue
+        self.lock = threading.Lock()#lock to keep our threads safe
+    def PUT(self, item):
+        self.empty.acquire()#we decrement our empty semaphore
+        self.lock.acquire()#lock so no one else can put while we are putting
+        self.queue.put(item)#put our frame
+        self.lock.release()#unlock
+        self.full.release()#full semaphore is incremented
+    def GET(self):
+        self.full.acquire()#full semaphore is decremented
+        self.lock.acquire()#lock so no one else can get while we are getting
+        frame = self.queue.get()#get our frame
+        self.lock.release()#unlock
+        self.empty.release()#empty semaphore is decremented
+        return frame#return our frame
+
 def extractFrames(fileName, outputBuffer, maxFramesToLoad=9999):
     # Initialize frame count 
     count = 0
@@ -25,24 +45,40 @@ def extractFrames(fileName, outputBuffer, maxFramesToLoad=9999):
         jpgAsText = base64.b64encode(jpgImage)
 
         # add the frame to the buffer
-        outputBuffer.put(image)
-       
+        outputBuffer.PUT(image)#place frame in our queue
+
         success,image = vidcap.read()
         print(f'Reading frame {count} {success}')
         count += 1
 
     print('Frame extraction complete')
 
+def convertFrames(inputBuffer, outputBuffer):
+    # initialize frame count
+    count = 0
+    inputFrame = inputBuffer.GET()#get from original frames
+    while inputFrame is not None and count < 72:
+        print(f'Converting frame {count}')
+        
+        # convert the image to grayscale
+        grayscaleFrame = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY)
+        count += 1
+
+        #queue next frame after converting
+        outputBuffer.PUT(grayscaleFrame)#put converted frame in our queue
+
+            
+        # load the next frame
+        inputFrame = inputBuffer.GET()#get from original frames
+    print("Done converting")
 
 def displayFrames(inputBuffer):
     # initialize frame count
     count = 0
 
+    frame = inputBuffer.GET()#get b&w frame
     # go through each frame in the buffer until the buffer is empty
-    while not inputBuffer.empty():
-        # get the next frame
-        frame = inputBuffer.get()
-
+    while frame is not None and count < 72:
         print(f'Displaying frame {count}')        
 
         # display the image in a window called "video" and wait 42ms
@@ -50,9 +86,10 @@ def displayFrames(inputBuffer):
         cv2.imshow('Video', frame)
         if cv2.waitKey(42) and 0xFF == ord("q"):
             break
-
         count += 1
 
+        # get the next frame
+        frame = inputBuffer.GET()
     print('Finished displaying all frames')
     # cleanup the windows
     cv2.destroyAllWindows()
@@ -61,11 +98,19 @@ def displayFrames(inputBuffer):
 filename = 'clip.mp4'
 
 # shared queue  
-extractionQueue = queue.Queue()
+extractionQueue = TObject()
 
-# extract the frames
-extractFrames(filename,extractionQueue, 72)
+# temporary queue
+convertQueue = TObject()
 
-# display the frames
-displayFrames(extractionQueue)
+t = threading.Thread(target = extractFrames, name = 'extract', args=(filename, extractionQueue, 72))
+t.start()
+
+t2 = threading.Thread(target = convertFrames, name = 'convert', args=(extractionQueue, convertQueue))
+t2.start()
+
+t3 = threading.Thread(target = displayFrames, name = 'display', args=(convertQueue,))
+t3.start()
+
+
 
